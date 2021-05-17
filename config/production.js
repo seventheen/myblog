bed38d12db70520d97bab66552dd0fce83915967
seventheen <1552939968@qq.com>
2021-05-17 22:38:06 +0800

    file

--- config/production.js
+++ config/production.js
@@ -1,8 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://zhaomin:zhaomin@cluster0.dsq0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const MongoClient = require('mongodb').MongoClient
+const uri = 'mongodb+srv://zhaomin:zhaomin@cluster0.dsq0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+    const collection = client.db('test').collection('devices')
+    client.close()
+})
+
+module.exports = {
+    port: 80
+}
--- index.js
+++ index.js
@@ -92,7 +92,12 @@ app.use(function(err, req, res, next) {
 
 
 // 监听端口，启动程序
-const port = process.env.PORT || config.port
-app.listen(port, function() {
-    console.log(`${pkg.name} listening on port ${port}`)
-})
+if (module.parent) {
+    // 被 require，则导出 app
+    module.exports = app
+} else {
+    // 监听端口，启动程序
+    app.listen(config.port, function() {
+        console.log(`${pkg.name} listening on port ${config.port}`)
+    })
+}
